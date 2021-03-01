651be1a90e03b7f57f6287deb255ffcd74eee549
matthewstclaire <matthew.crnich@gmail.com>
2021-03-01 09:31:20 -0700

    JS complete

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bg = document.querySelector('.bg');
 
 let load = 0;
 
-let int = setInterval(blurring, 15);
+let int = setInterval(blurring, 10);
 
 function blurring() {
   load++;
@@ -13,5 +13,11 @@ function blurring() {
   }
 
   loadText.innerText = `${load}%`;
-  loadText.style.opacity = 
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+
+//stack overflow method. https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
